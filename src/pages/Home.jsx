6e7fbd8a7f00490e9ad2f0548b38ff658b945d17
Dayan Arango <65741174+dayanMichelle@users.noreply.github.com>
2023-04-21 15:34:56 -0400

    creando componente de link (#4)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Link } from "../Link.jsx";
+
+export default function HomePage() {
+  return (
+    <>
+      <p>Esta es la home</p>
+      <Link to="/about">Ir a Sobre nosotros</Link>
+    </>
+  );
+}
